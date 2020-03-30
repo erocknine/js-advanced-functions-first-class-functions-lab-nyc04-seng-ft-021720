3b20be9c92b5520042ce64467a6aa4c6700e2e82
Sir Eric <echeungsk@gmail.com>
2020-03-30 17:39:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ const returnFirstTwoDrivers = function (drivers) { return drivers.slice(0, 2);};
 const returnLastTwoDrivers = function (drivers) {return drivers.slice(-2);};
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
 const createFareMultiplier = function (integer){ return function (value) { return value * integer;};};
-const fareDoubler = function (createFareMultiplier) { return createFareMultiplier * 2};
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
